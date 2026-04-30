a9c751ec6002aee5221098e8596681454d815be6
omardejesus14-oss <omardejesus14@gmail.com>
2026-04-30 10:37:47 -0500

    cambiado a middleware

---
+++ src/middleware.js
@@ -0,0 +1,22 @@
+
+import { NextResponse } from "next/server";
+import { createClient } from "./app/utils/supabase/server";
+
+
+export async function middleware(request) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*"],
+};
