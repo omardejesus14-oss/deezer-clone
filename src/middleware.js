3ae4e7d0f7ffa97ad0cae87119cbd43b4a4c3f8a
omardejesus14-oss <omardejesus14@gmail.com>
2026-04-30 22:35:52 -0500

    cambiando proxy

--- src/middleware.js
+++ src/middleware.js
@@ -1,16 +1,22 @@
 
 import { NextResponse } from "next/server";
-import { createClient } from "./app/utils/supabase/proxy";
+import { createClient } from "./app/utils/supabase/middleware";
+
+
 
 export async function middleware(request) {
   const { supabase, response } = createClient(request);
 
+  // IMPORTANTE: getUser() es más seguro que getSession() en el middleware para Vercel
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  // Si no hay usuario y trata de entrar al dashboard, redirigir
+  if (!user && request.nextUrl.pathname.startsWith("/dashboard")) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
   }
 
   return response;
