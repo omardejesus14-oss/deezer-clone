3f876a8d5ab2c5b49a81ad491f106b240c2f0760
omardejesus14-oss <omardejesus14@gmail.com>
2026-05-08 10:15:06 -0500

    quitando over en botones

--- src/app/components/music.jsx
+++ src/app/components/music.jsx
@@ -125,7 +125,7 @@ const query = search.toLowerCase().trim();
               />
 
               {/* BOTONES */}
-              <div className="absolute bottom-3 right-3 flex items-center gap-2 opacity-0 group-hover:opacity-100 transition">
+              <div className="absolute bottom-3 right-3 flex items-center gap-2 opacity-100 flex gap-2 transition md:opacity-0 md:group-hover:opacity-100 transition-opacity">
 
                 {/* PLAY */}
                 <button
